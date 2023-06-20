50ad175386e6f3feefab3ca34afc74cfd5e6506a
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-06-20 09:56:47 +0200

    added issue tracker ng prg

---
+++ issue-tracker/src/app/issue-list/issue-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
+import { IssuesService } from '../issues.service';
+
+@Component({
+  selector: 'app-issue-list',
+  templateUrl: './issue-list.component.html',
+  styleUrls: ['./issue-list.component.scss'],
+})
+export class IssueListComponent implements OnInit {
+  issues: Issue[] = [];
+  constructor(private issuesService: IssuesService) {}
+
+  ngOnInit(): void {
+    this.getIssues();
+  }
+
+  private getIssues() {
+    this.issues = this.issuesService.getPendingIssues();
+  }
+}
