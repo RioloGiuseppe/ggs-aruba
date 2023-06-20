3f9b2a0598ee6da3491559285bbea9e79a0bcd1d
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-06-20 18:07:16 +0200

    added suggestions

--- issue-tracker/src/app/issue-report/issue-report.component.ts
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-report',
@@ -11,6 +12,7 @@ export class IssueReportComponent implements OnInit {
 
   issueForm: FormGroup | undefined;
   @Output() formClose = new EventEmitter();
+  suggestions: Issue[] = []
 
   constructor(private builder: FormBuilder, private issuesServices: IssuesService) { }
 
@@ -21,10 +23,15 @@ export class IssueReportComponent implements OnInit {
       priority: ['', Validators.required],
       type: ['', Validators.required],
     });
+
+    this.issueForm.controls['title'].valueChanges.subscribe((text: string) => {
+      this.suggestions = this.issuesServices.getSuggestions(text)
+    })
+
   }
 
   addIssue() {
-    if(this.issueForm && this.issueForm.invalid){
+    if (this.issueForm && this.issueForm.invalid) {
       this.issueForm.markAllAsTouched();
       return;
     }
