2d0b76f62ef6e5e5976bbba96e2d3bb809eab6ba
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-06-20 16:58:27 +0200

    added issue report

---
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IssuesService } from '../issues.service';
+
+@Component({
+  selector: 'app-issue-report',
+  templateUrl: './issue-report.component.html',
+  styleUrls: ['./issue-report.component.scss']
+})
+export class IssueReportComponent implements OnInit {
+
+  issueForm: FormGroup | undefined;
+  @Output() formClose = new EventEmitter();
+
+  constructor(private builder: FormBuilder, private issuesServices: IssuesService) { }
+
+  ngOnInit(): void {
+    this.issueForm = this.builder.group({
+      title: ['', Validators.required],
+      description: [''],
+      priority: ['', Validators.required],
+      type: ['', Validators.required],
+    });
+  }
+
+  addIssue() {
+    this.issuesServices.createIssue(this.issueForm?.value)
+    this.formClose.emit()
+  }
+
+
+}
--- issue-tracker/src/app/issues.service.ts
+++ issue-tracker/src/app/issues.service.ts
@@ -8,9 +8,15 @@ import { issues } from 'src/assets/mockIssues';
 export class IssuesService {
   private issues: Issue[] = issues;
 
-  constructor() {}
+  constructor() { }
 
   getPendingIssues(): Issue[] {
     return this.issues.filter((issue) => !issue.completed);
   }
+
+  createIssue(issue: Issue) {
+    issue.issueNo = this.issues.length + 1
+    this.issues.push(issue)
+  }
+
 }
