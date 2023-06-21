68dbbe9e7f3c14a58709d07152b2bfb390f27c55
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2023-06-21 18:23:38 +0200

    added updates

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { filter, switchMap, map } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private updates: SwUpdate, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.updates.versionUpdates
+      .pipe(
+        filter((event): event is VersionReadyEvent => {
+          console.log(event);
+          const result = event.type === 'VERSION_READY';
+          return result;
+        }),
+        switchMap(() =>
+          this.snackBar.open('New version available', 'Reload')
+            .afterDismissed()),
+        filter(result => result.dismissedByAction),
+        map(() => this.updates.activateUpdate().then(() => document.location.reload()))
+      ).subscribe();
+  }
+
   title = 'weather-app';
 }
--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -6,16 +6,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { OwmIconPipe } from './shared';
 import { WeatherComponent } from './components';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from 'src/environments/environment';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherComponent,
-    OwmIconPipe
+    OwmIconPipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,11 +30,14 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatIconModule,
     MatCardModule,
     MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
+      enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      // or after 10 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:10000'
     })
   ],
   providers: [],
